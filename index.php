b00d9791b2f6af7d6ad6a0c02aa4cb65dc1af007
higaela <e.klh@hotmail.com>
2021-03-04 15:00:50 -0300

    7 Commit

--- index.php
+++ index.php
@@ -7,6 +7,5 @@ Burger do Zé
 
 <br>É um teste para aprender a desenvolver<br>
 
-<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fblog.novasafra.com.br%2F2018%2F07%2F21%2Fque-tal-inovar-nos-sabores-de-hamburguer-clique-aqui-e-descubra-como%2Fque-tal-inovar-nos-sabores-de-hamburguer-clique-aqui-e-descubra-como-jpeg%2F&psig=AOvVaw0UErNABoOunXSr-ZzSdI7m&ust=1614967047237000&source=images&cd=vfe&ved=2ahUKEwjdo9Wam5fvAhWiMrkGHQc_BzIQjRx6BAgAEAc">
-
+<img src="https://blog.lavillecasa.com.br/aprenda-como-fazer-hamburguer-gourmet/aprenda-como-fazer-hamburguer-gourmet-jpeg/">
 <br> teste
