1f2b00db6276fe1c77947694a459fe2394b5a389
higaela <e.klh@hotmail.com>
2021-03-04 14:53:58 -0300

    5 Commit

--- index.php
+++ index.php
@@ -5,5 +5,8 @@ Burger do Zé
 <br>Este site está sendo desenvolvido por Elaine Kelly
 <br>Treinamento GamaAcademy Turma 15A
 
-<br>É um teste para aprender a desenvolver
+<br>É um teste para aprender a desenvolver<br>
+
+<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fblog.lavillecasa.com.br%2Faprenda-como-fazer-hamburguer-gourmet%2Faprenda-como-fazer-hamburguer-gourmet-jpeg%2F&psig=AOvVaw2gs4WK0oiMZCNK6m7iQIT4&ust=1614966752214000&source=images&cd=vfe&ved=2ahUKEwibvv6NmpfvAhW7LbkGHZzLBFoQjRx6BAgAEAc"
+
 
